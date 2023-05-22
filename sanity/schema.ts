f471e4265ba0451e3510764c05be92bc9ab8e0ed
Robert Girardin <robertlgirardin@gmail.com>
2023-05-22 18:20:00 -0400

    remove post from studio

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -2,8 +2,8 @@ import { SchemaTypeDefinition } from 'sanity'
 
 import artist from './schemas/artist'
 import exhibit from './schemas/exhibit'
-import post from './schemas/post'
+//import post from './schemas/post'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [artist, exhibit, post],
+  types: [artist, exhibit],
 }
--- sanity/schemas/post.ts
+++ sanity/schemas/post.ts
@@ -1,16 +1,17 @@
 /* eslint-disable import/no-anonymous-default-export */
+import { defineField,defineType } from "sanity";
 
-export default {
+export default defineType({
   name: 'post',
   type: 'document',
   title: 'Post',
   fields: [
-    {
+    defineField({
       name: 'title',
       title: 'Title',
       type: 'string',
-    },
-    {
+    }),
+    defineField({
       type: 'slug',
       name: 'slug',
       title: 'Slug',
@@ -26,13 +27,13 @@ export default {
             .slice(0, 200),
       },
       validation: (Rule) => Rule.required(),
-    },
-    {
+    }),
+    defineField({
       name: 'publishedAt',
       title: 'Published at',
       type: 'datetime',
-    },
-    {
+    }),
+    defineField({
       name: 'body',
       title: 'Body',
       type: 'array',
@@ -41,11 +42,11 @@ export default {
           type: 'block',
         },
       ],
-    },
+    }),
   ],
   preview: {
     select: {
       title: 'title',
     },
   },
-}
+})
