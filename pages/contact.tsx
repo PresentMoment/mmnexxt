c2d21badb2f6551d8ebf93397efea0fc1e9f7684
Robert Girardin <robertlgirardin@gmail.com>
2023-05-29 13:33:55 -0400

    remove phone numbers

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -75,9 +75,6 @@ function Contact() {
         <span style={{ height: "1rem" }} /> Open by appointment only
         <span style={{ height: "1rem" }} /> <span style={{ height: "1rem" }} />
         <a href="mailto:mmxx.posta@gmail.com">mmxx.posta@gmail.com</a>
-        <span style={{ height: "1rem" }} />
-        <a href="tel:+39 346 811 69 78">EM +39 346 8116978</a> <span />{" "}
-        <a href="tel:+39 333 815 14 11">DM +39 333 8151411</a>{" "}
         <span style={{ height: "3rem" }} />
         Newsletter
       </p>
